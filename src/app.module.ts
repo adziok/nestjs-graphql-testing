3bcc403bf768bea7ee017e8ed5a2779bfc3f0c5a
Adrian Karwowski <adziok1998@gmail.com>
2022-04-07 20:54:20 +0200

    chore: init graphql nestjs project

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
