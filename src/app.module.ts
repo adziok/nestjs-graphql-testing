57d2c679fa7076118d183fd4d9848533b696bedc
Adrian Karwowski <adziok1998@gmail.com>
2022-04-07 21:03:24 +0200

    refactor: remove autogenerated files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
@@ -10,7 +9,6 @@ import { AppService } from './app.service';
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
